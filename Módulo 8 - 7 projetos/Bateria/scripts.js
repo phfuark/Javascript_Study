c77b20d048a9e36d1101b2f5da90fbce2e074040
phfuark <paulohsilva2007@gmail.com>
2024-12-25 20:47:46 -0300

    25.12.2024 20:47

--- Módulo 8 - 7 projetos/Bateria/scripts.js
+++ Módulo 8 - 7 projetos/Bateria/scripts.js
@@ -1,4 +1,5 @@
 
+
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 
 function playBuzzer(frequency, duration) {
@@ -20,14 +21,38 @@ const notes = {
 };
 
 
-document.querySelector('.q').addEventListener("click", () => playBuzzer(notes.do, 200));   // Do
-document.querySelector('.w').addEventListener("click", () => playBuzzer(notes.re, 200));   // Re
-document.querySelector('.e').addEventListener("click", () => playBuzzer(notes.mi, 200));   // Mi
-document.querySelector('.a').addEventListener("click", () => playBuzzer(notes.fa, 200));   // Fa
-document.querySelector('.s').addEventListener("click", () => playBuzzer(notes.sol, 200));  // Sol
-document.querySelector('.d').addEventListener("click", () => playBuzzer(notes.la, 200));   // La
-document.querySelector('.z').addEventListener("click", () => playBuzzer(notes.si, 200));   // Si
-document.querySelector('.x').addEventListener("click", () => playBuzzer(notes.do * 2, 200)); // High Do
-document.querySelector('.c').addEventListener("click", () => playBuzzer(notes.re * 2, 200)); // High Re
+document.querySelector('.q').addEventListener("click", () => playBuzzer(notes.do, 50));   // Do
+document.querySelector('.w').addEventListener("click", () => playBuzzer(notes.re, 50));   // Re
+document.querySelector('.e').addEventListener("click", () => playBuzzer(notes.mi, 50));   // Mi
+document.querySelector('.a').addEventListener("click", () => playBuzzer(notes.fa, 50));   // Fa
+document.querySelector('.s').addEventListener("click", () => playBuzzer(notes.sol, 50));  // Sol
+document.querySelector('.d').addEventListener("click", () => playBuzzer(notes.la, 50));   // La
+document.querySelector('.z').addEventListener("click", () => playBuzzer(notes.si, 50));   // Si
+document.querySelector('.x').addEventListener("click", () => playBuzzer(notes.do * 2, 50)); // High Do
+document.querySelector('.c').addEventListener("click", () => playBuzzer(notes.re * 2, 50)); // High Re
+
+document.addEventListener('keyup', press);
+
+function press(evento) {
+    if (evento.key == 'q' || evento.key == 'Q'){
+        playBuzzer(notes.do, 50);
+    } else if (evento.key == 'w' || evento.key == 'W'){
+        playBuzzer(notes.re, 50);
+    } else if (evento.key == 'e' || evento.key == 'E'){
+        playBuzzer(notes.mi, 50);
+    } else if (evento.key == 'a' || evento.key == 'A'){
+        playBuzzer(notes.fa, 50);
+    } else if (evento.key == 's' || evento.key == 'S'){
+        playBuzzer(notes.sol, 50);
+    } else if (evento.key == 'd' || evento.key == 'D'){
+        playBuzzer(notes.la, 50);
+    } else if (evento.key == 'z' || evento.key == 'Z'){
+        playBuzzer(notes.si, 50);
+    } else if (evento.key == 'x' || evento.key == 'X'){
+        playBuzzer(notes.do * 2, 50);
+    } else if (evento.key == 'c' || evento.key == 'C'){
+        playBuzzer(notes.re * 2, 50);
+    } 
+}
 
 // playBuzzer(440, 500); 
